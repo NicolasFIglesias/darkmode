ceae19105f1d85afc8226c30c50e7c03f6dc9b29
Nícolas Iglesias <101362189+NicolasFIglesias@users.noreply.github.com>
2023-03-14 01:01:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const changeThemeBtn = document.querySelector('#change-theme')
+
+function toggleDarkMode(){
+    document.body.classList.toggle('dark')
+}
+
+
+function loadTheme(){
+    const darkMode = localStorage.getItem('dark')
+
+    if(darkMode){
+        toggleDarkMode()
+    }
+}
+
+changeThemeBtn.addEventListener('change', function(){
+    toggleDarkMode()
+
+    localStorage.removeItem('dark')
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark', 1)       
+     }
+  
+})
+ loadTheme()
+
